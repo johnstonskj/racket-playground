fe77b5f46d9c2f4ebc7fa21e34e5ea4b190b862a
Simon Johnston <johnstonskj@gmail.com>
2018-06-03 11:28:47 -0700

    Adding at-exp example for LaTeX

---
+++ examples/rackex.rkt
@@ -0,0 +1,20 @@
+#lang at-exp racket
+
+(module rackex/base racket
+
+  (provide document-class & textit)
+
+  (define (document-class class-name) (display ""))
+
+  (define (& t . tt) (display t) (display (apply string-append tt)))
+
+  (define (textit t) (format "~a" t))
+)
+
+(require 'rackex/base)
+
+@document-class[]{article}
+
+@&{here is some @textit{italic} text}
+
+(displayln "")
